d9fc71204f097e1630703887360f42078dd182dc
Nathan Lewis <nathan@differential.com>
2023-12-01 10:19:50 -0600

    chore: solved day 1 part 2

--- src/aoc2023/aoc1.js
+++ src/aoc2023/aoc1.js
@@ -1,29 +1,78 @@
 const { aoc_input } = require("../../config");
 const fs = require("fs");
 
-// Part 1
-// On each line, return the first and last number and combine them to make a new 2 digit number
-// Add all these numbers together
+// Helper function to convert spelled-out numbers to digits
+function convertSpelledOutToDigits(line) {
+  const SPELLED_OUT_NUMBERS = {
+    one: "o1e",
+    two: "t2o",
+    three: "t3e",
+    four: "f4r",
+    five: "f5e",
+    six: "s6x",
+    seven: "s7n",
+    eight: "e8t",
+    nine: "n9e",
+  };
 
+  let numbers = line;
+
+  for (const [key, value] of Object.entries(SPELLED_OUT_NUMBERS)) {
+    numbers = numbers.replace(new RegExp(key, "g"), value);
+  }
+
+  return numbers;
+}
+
+// Helper function to calculate the sum of combined digits in a line
+function calculateCombinedDigitSum(line) {
+  const digits = line.match(/\d/g);
+
+  if (!digits) return 0;
+
+  const firstDigit = digits[0].toString();
+  const lastDigit = digits[digits.length - 1].toString();
+  const combinedDigits = parseInt(firstDigit + lastDigit);
+
+  return combinedDigits;
+}
+
+// Part 1
 function part1(lines) {
   let total = 0;
 
   for (const line of lines) {
-    const numbers = line.match(/\d+/g);
-    if (numbers) {
-      const firstDigit = numbers[0].toString();
-      const lastDigit = numbers[numbers.length - 1].toString();
-      const combinedDigits = parseInt(firstDigit + lastDigit);
+    const numbers = convertSpelledOutToDigits(line);
+    const combinedDigits = calculateCombinedDigitSum(numbers);
 
-      total += combinedDigits;
-    }
+    total += combinedDigits;
   }
 
   return total;
 }
 
-// Reading from file and running part1
-const lines = fs.readFileSync(aoc_input, "utf-8").split("\n");
-console.log(part1(lines));
+// Part 2
+function part2(lines) {
+  let total = 0;
 
-module.exports = { part1 };
+  for (const line of lines) {
+    const numbers = convertSpelledOutToDigits(line);
+    const combinedDigits = calculateCombinedDigitSum(numbers);
+
+    total += combinedDigits;
+  }
+
+  return total;
+}
+
+// Reading from file and running both parts
+const lines = fs.readFileSync(aoc_input, "utf-8").split("\n");
+console.log("Part 1:", part1(lines));
+console.log("Part 2:", part2(lines));
+
+module.exports = {
+  part1,
+  part2,
+  convertSpelledOutToDigits,
+  calculateCombinedDigitSum,
+};
--- tests/unit/aoc2023/aoc1.jest.test.js
+++ tests/unit/aoc2023/aoc1.jest.test.js
@@ -1,4 +1,9 @@
-const { part1 } = require("../../../src/aoc2023/aoc1");
+const {
+  part1,
+  part2,
+  convertSpelledOutToDigits,
+  calculateCombinedDigitSum,
+} = require("../../../src/aoc2023/aoc1");
 const { test, expect } = require("@jest/globals");
 
 test("part 1 processes lines correctly", () => {
@@ -6,4 +11,62 @@ test("part 1 processes lines correctly", () => {
 
   const expectedResult = 142; // sum of combined digits
   expect(part1(mockLines)).toBe(expectedResult);
+});
+
+test("calculateCombinedDigitSum calculates the sum of combined digits in a line", () => {
+  const mockLines = [
+    "219",
+    "823",
+    "abc123xyz",
+    "x2134",
+    "49872",
+    "z18234",
+    "7pqrst6teen",
+  ];
+
+  const expectedResult = [29, 83, 13, 24, 42, 14, 76];
+  expect(mockLines.map((line) => calculateCombinedDigitSum(line))).toEqual(
+    expectedResult
+  );
+});
+
+test("convertSpelledOutToDigits converts spelled out numbers to digits", () => {
+  const mockLines = [
+    "two1nine",
+    "eightwothree",
+    "abcone2threexyz",
+    "xtwone3four",
+    "4nineeightseven2",
+    "zoneight234",
+    "7pqrstsixteen",
+  ];
+
+  const expectedResult = [
+    "t2o1n9e",
+    "e8t2ot3e",
+    "abco1e2t3exyz",
+    "xt2o1e3f4r",
+    "4n9ee8ts7n2",
+    "zo1e8t234",
+    "7pqrsts6xteen",
+  ];
+
+  expect(mockLines.map((line) => convertSpelledOutToDigits(line))).toEqual(
+    expectedResult
+  );
+});
+
+test("part 2 processes lines correctly", () => {
+  const mockLines = [
+    "two1nine", // 29
+    "eightwothree", // 83
+    "abcone2threexyz", // 13
+    "xtwone3four", // 24
+    "4nineeightseven2", // 42
+    "zoneight234", // 14
+    "7pqrstsixteen", // 76
+  ];
+
+  const expectedResult = 29 + 83 + 13 + 24 + 42 + 14 + 76; // sum of combined digits
+  expect(part2(mockLines)).toBe(expectedResult);
 });
