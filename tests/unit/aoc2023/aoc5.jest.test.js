320c87dde8158bb5c58fade791769113436a8a75
Nathan Lewis <nathan@differential.com>
2023-12-05 08:31:26 -0600

    chore: passing tests for map function

--- tests/unit/aoc2023/aoc5.jest.test.js
+++ tests/unit/aoc2023/aoc5.jest.test.js
@@ -3,7 +3,7 @@ const {
   part2,
   parseInput,
   mapNumber,
-} = require("../../../src/aoc2023/aoc4");
+} = require("../../../src/aoc2023/aoc5");
 const { aoc_input, aoc_test_input } = require("../../../config");
 const { test, expect } = require("@jest/globals");
 const fs = require("fs");
@@ -11,45 +11,59 @@ const fs = require("fs");
 test("parseInput parses the input correctly", () => {
   const mockInput = fs.readFileSync(aoc_test_input, "utf-8");
 
-  const expectedResult = [
-    "seeds: 79 14 55 13",
-    "seed-to-soil map:",
-    "50 98 2",
-    "52 50 48",
-    "soil-to-fertilizer map:",
-    "0 15 37",
-    "37 52 2",
-    "39 0 15",
-    "fertilizer-to-water map:",
-    "49 53 8",
-    "0 11 42",
-    "42 0 7",
-    "57 7 4",
-    "water-to-light map:",
-    "88 18 7",
-    "18 25 70",
-    "light-to-temperature map:",
-    "45 77 23",
-    "81 45 19",
-    "68 64 13",
-    "temperature-to-humidity map:",
-    "0 69 1",
-    "1 0 69",
-    "humidity-to-location map:",
-    "60 56 37",
-    "56 93 4",
-  ];
+  const expectedResult = {
+    mappings: [
+      [
+        [50, 98, 2],
+        [52, 50, 48],
+      ],
+      [
+        [0, 15, 37],
+        [37, 52, 2],
+        [39, 0, 15],
+      ],
+      [
+        [49, 53, 8],
+        [0, 11, 42],
+        [42, 0, 7],
+        [57, 7, 4],
+      ],
+      [
+        [88, 18, 7],
+        [18, 25, 70],
+      ],
+      [
+        [45, 77, 23],
+        [81, 45, 19],
+        [68, 64, 13],
+      ],
+      [
+        [0, 69, 1],
+        [1, 0, 69],
+      ],
+      [
+        [60, 56, 37],
+        [56, 93, 4],
+      ],
+    ],
+    seeds: [79, 14, 55, 13],
+  };
 
   expect(parseInput(mockInput)).toEqual(expectedResult);
 });
 
 test("mapNumber maps numbers correctly", () => {
-  const mockNumber = 100;
+  const mockNumber = 0;
+  const mockNumber2 = 50;
+  const mockNumber3 = 51;
+  const mockNumber4 = 98;
   const mockMapping = [
-    [0, 0, 50],
-    [50, 50, 50],
-    [100, 100, 50],
+    [50, 98, 2],
+    [52, 50, 48],
   ];
 
-  expect(mapNumber(mockNumber, mockMapping)).toBe(150);
+  expect(mapNumber(mockNumber, mockMapping)).toBe(0);
+  expect(mapNumber(mockNumber2, mockMapping)).toBe(52);
+  expect(mapNumber(mockNumber3, mockMapping)).toBe(53);
+  expect(mapNumber(mockNumber4, mockMapping)).toBe(50);
 });
