5d809236feccee973640b1dff0a28f952ab83486
Nathan Lewis <nathan@differential.com>
2023-12-13 17:46:41 -0600

    feat: solved day 13 part 1 with passing tests

--- src/aoc2023/aoc13.js
+++ src/aoc2023/aoc13.js
@@ -13,57 +13,120 @@ function parseInput(input) {
 }
 
 function findReflectionLine(pattern) {
-  // Input: Array<string>; ex: ["#",".","#","#",".",".","#","#","."]
+  // Input: Array of array of strings; ex: [["#", ".", "#", "#", ".", ".", "#", "#", "."], [".", ".", "#", ".", "#", "#", ".", "#", "."], ["#", "#", ".", ".", ".", ".", ".", ".", "#"], ["#", "#", ".", ".", ".", ".", ".", ".", "#"], [".", ".", "#", ".", "#", "#", ".", "#", "."], [".", ".", "#", "#", ".", ".", "#", "#", "."], ["#", ".", "#", ".", "#", "#", ".", "#", "."]]
   // Output: {type: string, index: number}; ex: {type: "vertical", index: 3}
+  const numRows = pattern.length;
+  const numCols = pattern[0].length;
+
+  // Create strings for each row
+  const rowStrings = pattern.map((row) => row.join(""));
+
+  // Check for horizontal reflection
+  for (let row = 0; row < numRows - 1; row++) {
+    if (rowStrings[row] === rowStrings[row + 1]) {
+      // Check pairs expanding outwards from this point
+      let isReflection = true;
+      for (
+        let offset = 1;
+        row - offset >= 0 && row + 1 + offset < numRows;
+        offset++
+      ) {
+        if (rowStrings[row - offset] !== rowStrings[row + 1 + offset]) {
+          isReflection = false;
+          break;
+        }
+      }
+      if (isReflection) {
+        return { type: "horizontal", index: row };
+      }
+    }
+  }
+
+  // Create strings for each column
+  const colStrings = Array(numCols)
+    .fill("")
+    .map((_, colIndex) => pattern.map((row) => row[colIndex]).join(""));
+
+  // Check for vertical reflection
+  for (let col = 0; col < numCols - 1; col++) {
+    if (colStrings[col] === colStrings[col + 1]) {
+      // Check pairs expanding outwards from this point
+      let isReflection = true;
+      for (
+        let offset = 1;
+        col - offset >= 0 && col + 1 + offset < numCols;
+        offset++
+      ) {
+        if (colStrings[col - offset] !== colStrings[col + 1 + offset]) {
+          isReflection = false;
+          break;
+        }
+      }
+      if (isReflection) {
+        return { type: "vertical", index: col };
+      }
+    }
+  }
+
+  return null;
 }
 
-function isPatternSymmetric(pattern, reflectionLine) {
-  // Implementation: Compare the segments of the pattern based on the reflection line.
-  // Input: Array<string>; ex: ["#",".","#","#",".",".","#","#","."]
-  // Input: {type: string, index: number}; ex: {type: "vertical", index: 3}
-  // Output: boolean; ex: true
-}
-
-/**
- * Calculates the score based on the reflection line.
- * @param {{type: string, index: number}} reflectionLine - The reflection line information.
- * @return {number} The calculated score.
- */
 function calculateScore(reflectionLine) {
   // Input: {type: string, index: number}; ex: {type: "vertical", index: 3}
   // Output: number; ex: 3
+  if (reflectionLine.type === "vertical") {
+    // Score is the index of the vertical line (number of columns to the left)
+    return reflectionLine.index + 1;
+  } else if (reflectionLine.type === "horizontal") {
+    // Score is 100 times the index of the horizontal line (number of rows above)
+    // Increase the index by 1 to account for the top row
+    const index = reflectionLine.index + 1;
+    return 100 * index;
+  }
+
+  // If no reflection line, return 0 or handle as needed
+  return 0;
 }
 
 function summarizePatterns(patterns) {
   // Input: Array<Array<string>>; ex: [["#",".","#","#",".",".","#","#","."],[".",".","#",".","#","#",".","#","."],["#","#",".",".",".",".",".","#"]]
   // Output: number; ex: 6
-}
-
-function main() {
-  // 1. Read the input.
-  // 2. Parse the input using parseInput.
-  // 3. Iterate over each pattern, process them, and calculate the total score using summarizePatterns.
-  // 4. Output the final result.
+  return patterns.reduce((totalScore, pattern) => {
+    const reflectionLine = findReflectionLine(pattern);
+    if (reflectionLine) {
+      return totalScore + calculateScore(reflectionLine);
+    }
+    return totalScore;
+  }, 0);
 }
 
 // Part 1
 function part1(lines) {
-  return 0;
+  const patterns = parseInput(lines);
+  const totalScore = summarizePatterns(patterns);
+
+  return totalScore;
 }
 
 // Part 2
 function part2(lines) {
-  return 0;
+  const patterns = parseInput(lines);
+  const totalScore = summarizePatterns(patterns);
+
+  return totalScore;
 }
 
 // Reading from file and running both parts
-const lines = fs.readFileSync(aoc_input, "utf-8").split("\n");
+const lines = fs.readFileSync(aoc_input, "utf-8");
 // console.log("Part 1:", part1(lines));
-// console.log("Part 2:", part2(lines));
+console.log("Part 2:", part2(lines));
 
 module.exports = {
   part1,
   part2,
   // other functions
   parseInput,
+  findReflectionLine,
+  calculateScore,
+  summarizePatterns,
 };
