1055949ccb146287f9c97635b62d4acf5f830660
Nathan Lewis <nathan@differential.com>
2023-12-12 21:05:51 -0600

    part 1 solved for day 12

---
+++ src/aoc2023/aoc12.js
@@ -0,0 +1,147 @@
+const { aoc_input, aoc_test_input } = require("../../config");
+const fs = require("fs");
+
+// Helper functions
+function parseInput(input) {
+  // Input: string of lines; ex: "???.### 1,1,3\n.??..??...?##. 1,1,3"
+  // Output: array of objects; ex: [ { springPattern: "???.###", groupSizes: [1, 1, 3] }, { springPattern: ".??..??...?##.", groupSizes: [1, 1, 3] } ]
+
+  return input.split("\n").map((line) => {
+    let [springPattern, groupSizesStr] = line.split(" ");
+    let groupSizes = groupSizesStr.split(",").map(Number);
+    return { springPattern, groupSizes };
+  });
+}
+
+function initializeArrangementCounts(patternLength, groupCount) {
+  // Input: pattern length, group count; ex: 8, 3
+  // Output: 3D array; ex: [ [ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ... ], ... ]
+
+  // Needed a way to track three things: current position in the pattern, current testing group, and size of previous group
+  return Array.from({ length: patternLength + 1 }, () =>
+    Array.from({ length: groupCount + 2 }, () =>
+      new Array(patternLength + 2).fill(0)
+    )
+  );
+}
+
+function calculateArrangements(springPattern, groupSizes) {
+  // Input: spring pattern, group sizes; ex: "???.###", [1, 1, 3]
+  // Output: number; ex: 4
+  springPattern += ".";
+  let patternLength = springPattern.length;
+  let groupCount = groupSizes.length;
+  groupSizes.push(patternLength + 1);
+
+  let arrangementCounts = initializeArrangementCounts(
+    patternLength,
+    groupCount
+  );
+  arrangementCounts[0][0][0] = 1;
+
+  for (let patternIndex = 0; patternIndex < patternLength; patternIndex++) {
+    for (
+      let currentGroupIndex = 0;
+      currentGroupIndex < groupCount + 1;
+      currentGroupIndex++
+    ) {
+      for (
+        let sizeOfPreviousGroup = 0;
+        sizeOfPreviousGroup < patternLength + 1;
+        sizeOfPreviousGroup++
+      ) {
+        let countAtCurrentState =
+          arrangementCounts[patternIndex][currentGroupIndex][
+            sizeOfPreviousGroup
+          ];
+        if (!countAtCurrentState) continue;
+
+        updateArrangementCounts(
+          arrangementCounts,
+          springPattern,
+          groupSizes,
+          patternIndex,
+          currentGroupIndex,
+          sizeOfPreviousGroup,
+          countAtCurrentState
+        );
+      }
+    }
+  }
+
+  return arrangementCounts[patternLength][groupCount][0];
+}
+
+function updateArrangementCounts(
+  arrangementCounts,
+  springPattern,
+  groupSizes,
+  patternIndex,
+  currentGroupIndex,
+  sizeOfPreviousGroup,
+  countAtCurrentState
+) {
+  // Input: arrangement counts, spring pattern, group sizes, pattern index, current group index, size of previous group, count at current state; ex: [ [ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ... ], ... ], "???.###", [1, 1, 3], 0, 0, 0, 1
+  // Output: updated 3d array - not returned
+  if (
+    springPattern[patternIndex] === "." ||
+    springPattern[patternIndex] === "?"
+  ) {
+    if (
+      sizeOfPreviousGroup === 0 ||
+      sizeOfPreviousGroup === groupSizes[currentGroupIndex - 1]
+    ) {
+      arrangementCounts[patternIndex + 1][currentGroupIndex][0] +=
+        countAtCurrentState;
+    }
+  }
+  if (
+    springPattern[patternIndex] === "#" ||
+    springPattern[patternIndex] === "?"
+  ) {
+    arrangementCounts[patternIndex + 1][
+      currentGroupIndex + (sizeOfPreviousGroup === 0 ? 1 : 0)
+    ][sizeOfPreviousGroup + 1] += countAtCurrentState;
+  }
+}
+
+function countArrangements(input) {
+  // Input: string of lines; ex: "???.### 1,1,3\n.??..??...?##. 1,1,3"
+  // Output: number; ex: 8
+  let totalArrangements = 0;
+  const parsedData = parseInput(input);
+
+  parsedData.forEach(({ springPattern, groupSizes }) => {
+    totalArrangements += calculateArrangements(springPattern, groupSizes);
+  });
+
+  return totalArrangements;
+}
+
+// Part 1
+function part1(lines) {
+  const solution = countArrangements(lines);
+  return solution;
+}
+
+// Part 2
+function part2(lines) {
+  return 0;
+}
+
+// Reading from file and running both parts
+const lines = fs.readFileSync(aoc_input, "utf-8");
+console.log("Part 1:", part1(lines));
+// console.log("test", countArrangements("???.### 1,1,3"));
+// console.log("Part 2:", part2(lines));
+
+module.exports = {
+  part1,
+  part2,
+  // other functions
+  parseInput,
+  initializeArrangementCounts,
+  calculateArrangements,
+  updateArrangementCounts,
+  countArrangements,
+};
---
+++ tests/unit/aoc2023/aoc12.jest.test.js
@@ -0,0 +1,122 @@
+const {
+  part1,
+  part2,
+  parseInput,
+  initializeArrangementCounts,
+  calculateArrangements,
+  updateArrangementCounts,
+} = require("../../../src/aoc2023/aoc12");
+const { aoc_input, aoc_test_input } = require("../../../config");
+const { test, expect } = require("@jest/globals");
+const fs = require("fs");
+
+test("parseInput", () => {
+  const input = `???.### 1,1,3\n.??..??...?##. 1,1,3`;
+
+  const expected = [
+    { springPattern: "???.###", groupSizes: [1, 1, 3] },
+    { springPattern: ".??..??...?##.", groupSizes: [1, 1, 3] },
+  ];
+
+  expect(parseInput(input)).toEqual(expected);
+});
+
+test("initializeArrangementCounts", () => {
+  const patternLength = 8;
+  const groupCount = 3;
+  const expected = [
+    [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+  ];
+
+  expect(initializeArrangementCounts(patternLength, groupCount)).toEqual(
+    expected
+  );
+});
+
+test("calculateArrangements", () => {
+  const springPattern = "???.###";
+  const groupSizes = [1, 1, 3];
+  const expected = 1;
+
+  expect(calculateArrangements(springPattern, groupSizes)).toEqual(expected);
+
+  const springPattern2 = ".??..??...?##.";
+  const groupSizes2 = [1, 1, 3];
+  const expected2 = 4;
+
+  expect(calculateArrangements(springPattern2, groupSizes2)).toEqual(expected2);
+
+  const springPattern3 = "?#?#?#?#?#?#?#?";
+  const groupSizes3 = [1, 3, 1, 6];
+  const expected3 = 1;
+
+  expect(calculateArrangements(springPattern3, groupSizes3)).toEqual(expected3);
+
+  const springPattern4 = "????.######..#####.";
+  const groupSizes4 = [1, 6, 5];
+  const expected4 = 4;
+
+  expect(calculateArrangements(springPattern4, groupSizes4)).toEqual(expected4);
+});
